e118b0a14f2ced9f4a36e6920b6af11fd1dce464
henriquemota <mota.henrique@gmail.com>
2023-03-30 08:19:02 -0300

    inicializacao do projeto

---
+++ exemplo-php.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Meu primeiro PHP <?php echo('mudei o titulo') ?></title>
+</head>
+<body>
+  <p><?php echo('ola eu aqui') ?></p>
+  <p><?php echo('ola eu aqui novamente') ?></p>
+  <p><?php echo(1 + 5 + 8 + 7) ?></p>
+  <p><?php echo($_GET['nome']) ?></p>
+</body>
+</html>
